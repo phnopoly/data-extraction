6cb3f68215529eee1c22fcae10e3570bd2faf64e
Phong <pvo5@masonlive.gmu.edu>
2018-10-19 11:45:09 -0400

    new class

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,14 @@
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import java.io.FileInputStream;
 
 public class Driver {
+
+
+    private static FileInputStream file;
+    private static HSSFWorkbook workbook;
+    static HSSFSheet staticData;
+    private static HSSFSheet dynamicData;
 
     public static void main(String[] args){
 
